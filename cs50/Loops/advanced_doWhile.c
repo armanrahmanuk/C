7fa484a40d2422a73c43ce685cfc6ba536c79e6a
armanrahmanbd <armanrahmanbd7@gmail.com>
2026-02-04 17:28:16 +0000

    Final version of code

--- cs50/Loops/advanced_doWhile.c
+++ cs50/Loops/advanced_doWhile.c
@@ -1,35 +1,14 @@
 #include <stdio.h>
-
-void get_name(char name[]);
-int get_times(void);
-void print_name(int times, char name);
+#include <string.h>
 
 int main(void)
 {
-  char name[20];
-  int times;
-
-  get_name(name);
-  times = get_times();
-  print_name(times, name);
-}
-
-void get_name(char name[])
-{
+  char name[20] = "";
+  int times = 0;
   printf("Give a word to print: ");
   scanf("%s", name);
-}
-
-int get_times(void)
-{
-  int times = 0;
   printf("How many times?: ");
   scanf("%d", &times);
-  return times;
-}
-
-void print_name(int times, char name)
-{
   for (int i = 0; i < times; i++)
   {
     printf("%s\n", name);
