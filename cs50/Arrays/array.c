833919ad4cdd110108ffe37edd97b4fbbb9610d8
armanrahmanbd <armanrahmanbd7@gmail.com>
2026-02-04 15:28:57 +0000

    Making the array more flexible by calculating its size dynamically.

--- cs50/Arrays/array.c
+++ cs50/Arrays/array.c
@@ -3,8 +3,10 @@
 int main(void)
 {
 
-  int numbers[] = {6, 7, 8, 9, 90};
-  for (int i = 0; i < 5; i++)
+  int numbers[] = {6, 7, 8, 9, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+  int count = sizeof(numbers) / sizeof(numbers[0]);
+
+  for (int i = 0; i < count; i++)
   {
     printf("%d ", numbers[i]);
   }
