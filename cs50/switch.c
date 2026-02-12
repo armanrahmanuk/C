1f46347ff712c67d9d9ce7a9cdf4f1b72fa535d5
armanrahmanbd <armanrahmanbd7@gmail.com>
2026-02-12 01:57:24 +0000

    Leraned about switch statements in C. They allow you to execute different code based on the value of a variable. It's like a more organized way of writing multiple if-else statements. You can use it to handle different cases more efficiently.

---
+++ cs50/switch.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+  double num1 = 0.0;
+  double num2 = 0.0;
+  char operator = '\0';
+  printf("Give a number: ");
+  scanf("%lf", &num1);
+  do
+  {
+    printf("Which operation do you wanna use (+ - * /): ");
+    scanf(" %c", &operator);
+  } while (operator != '+' && operator != '-' && operator != '*' && operator != '/');
+
+  printf("Give the 2nd number: ");
+  scanf("%lf", &num2);
+
+  double result = 0.0;
+  {
+
+    switch (operator)
+    {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+      break;
+
+    default:
+      break;
+    }
+  }
+  printf("The result is: %.2lf\n", result);
+}
